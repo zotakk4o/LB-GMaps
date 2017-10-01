3a866de1cda02b2e9e8f9ad0df21b31f35c091bb
zotakk4o <liubomir.borisov@abv.bg>
2017-10-01 11:40:01 +0300

    Add controls options

--- assets/js/lb_gmaps_live_preview.js
+++ assets/js/lb_gmaps_live_preview.js
@@ -36,7 +36,7 @@ function initMap() {
         }
     }
 
-    map.addListener( 'click', function ( event ) {
+    map.addListener( 'dblclick', function ( event ) {
         createMarker( map, event.latLng );
     } );
 
@@ -99,170 +99,6 @@ function initMap() {
         } );
     } );
 
-    function styleInfowindow( infowindow ) {
-        google.maps.event.addListener( infowindow, 'domready', function() {
-
-            // Reference to the DIV that wraps the bottom of infowindow
-            var iwOuter = $('.gm-style-iw');
-            iwOuter.parent().css({'width': iwOuter.css('width'), 'height': iwOuter.css('height') });
-
-            /* Since this div is in a position prior to .gm-div style-iw.
-             * We use jQuery and create a iwBackground variable,
-             * and took advantage of the existing reference .gm-style-iw for the previous div with .prev().
-             */
-            var iwBackground = iwOuter.prev();
-
-            // Removes background shadow DIV
-            iwBackground.children(':nth-child(2)').css({'display' : 'none'});
-
-            // Removes white background DIV
-            iwBackground.children(':nth-child(4)').css({'display' : 'none'});
-
-            // Changes the desired tail shadow color.
-            iwBackground.children(':nth-child(3)').find('div').children().css({'box-shadow': 'rgba(72, 181, 233, 0.6) 0px 1px 6px', 'z-index' : '1'});
-
-            // Reference to the div that groups the close button elements.
-            var iwCloseBtn = iwOuter.next();
-
-            // Apply the desired effect to the close button
-            iwCloseBtn.css({opacity: '1', top: '0' , right: '-35px', border: '7px solid #417DF1', 'border-radius': '13px', 'box-shadow': '0 0 5px #3990B9'});
-
-            // If the content of infowindow not exceed the set maximum height, then the gradient is removed.
-            if($('#lb-gmaps-marker-description').height() < 140){
-                $('#lb-gmaps-marker-gradient ').css({display: 'none'});
-            }
-
-            // The API automatically applies 0.7 opacity to the button after the mouseout event. This function reverses this event to the desired value.
-            iwCloseBtn.mouseout(function(){
-                $(this).css({opacity: '1'});
-            });
-        });
-    }
-
-    function showMarkerForm( map, marker ) {
-        var markerObject = {
-            uniqueness: marker.uniqueness,
-            post_id: post.ID,
-            lat: marker.position.lat(),
-            lng: marker.position.lng()
-        };
-
-        var markerForm = $( views.form );
-
-        if( marker.hasOwnProperty( 'name' ) ) {
-            markerForm.find( '#marker_name' ).val( marker.name );
-        }
-        if( marker.hasOwnProperty( 'content' ) ) {
-            markerForm.find( '#marker_description' ).val( marker.content );
-        }
-        if( ! marker.hasOwnProperty( 'name' ) && ! marker.hasOwnProperty( 'content' ) ) {
-            $( '<button type="button" id="delete-button">Delete Marker</button>' ).insertAfter( markerForm.find( '#save-button' ) );
-        }
-
-
-        markerForm.insertAfter( '#lb-gmaps-fields' );
-
-        validateMarkerForm( markerForm );
-
-        $( '#cancel-button' ).on( 'click', function () {
-            markerForm.remove();
-            displayInfoWindow( map, marker );
-        } );
-
-        $( '#save-button' ).on( 'click', function () {
-            markerObject.name = $.trim( $( '#marker_name' ).val() );
-            markerObject.content = $.trim( $( '#marker_description' ).val() );
-            markerForm.remove();
-            $.ajax( {
-                type: "POST",
-                url: admin.ajaxURL,
-                data: {
-                    action: 'save_marker_data',
-                    marker: markerObject
-                }
-            } ).then( function (data) {
-                if( data ) {
-                    marker.name = markerObject.name;
-                    marker.content = markerObject.content;
-                    displayInfoWindow( map, marker );
-                }
-            } );
-        } );
-
-        $( '#delete-button' ).on( 'click', function () {
-            marker.setMap( null );
-            markerForm.remove();
-        } )
-    }
-
-    function addMarkerClickListener( map, marker, content, infoWindow ) {
-        google.maps.event.clearListeners( marker, 'click' );
-        google.maps.event.addListener( marker, 'click', ( function( marker, content, infoWindow ) {
-            return function() {
-                if( undefined !== infoWindow && undefined !== infoWindow && undefined !== content ) {
-                    infoWindow.setContent( content );
-                    if( undefined === infoWindow.getMap() || null === infoWindow.getMap() ) {
-                        infoWindow.open( map, marker );
-                    }
-
-                    $( '#edit-lb-gmaps-marker' ).on( 'click', function (  ) {
-                        $( '.gm-style-iw' ).parent().remove();
-                        showMarkerForm( map, marker );
-                    } );
-
-                    $(' #delete-lb-gmaps-marker ').on( 'click', function () {
-                        $( '.gm-style-iw' ).parent().remove();
-                        google.maps.event.clearListeners( marker, 'click' );
-                        var markerObject = {
-                            uniqueness: marker.uniqueness,
-                            lat: marker.position.lat(),
-                            lng: marker.position.lng()
-                        };
-                        $.ajax( {
-                            type: 'POST',
-                            url: admin.ajaxURL,
-                            data: {
-                                action: 'delete_marker_data',
-                                marker: markerObject
-                            }
-                        } ).then( function ( data ) {
-                            if( data ) {
-                                marker.setMap( null );
-                            }
-                        } )
-                    } )
-                } else {
-                    showMarkerForm( map, marker );
-                }
-
-            };
-        } )( marker, content, infoWindow) );
-    }
-
-    function displayInfoWindow( map, marker ) {
-        var content = $( views.infoBox );
-        content.find( '#lb-gmaps-marker-description' ).append( '<span id="edit-lb-gmaps-marker">Edit Marker Info</span>' );
-        content.find( '#lb-gmaps-marker-description' ).append( '<span id="delete-lb-gmaps-marker">Delete Marker</span>' );
-        if( null !== marker.name && null !== marker.content && undefined !== marker.name && undefined !== marker.content) {
-            if( null !== marker.name ) {
-                content.find( '#lb-gmaps-marker-name p' ).text( marker.name );
-            }
-            if( null !== marker.content ) {
-                content.find( '#lb-gmaps-marker-description p' ).text( marker.content );
-            }
-            var textContent = content[0].outerHTML;
-            var infoWindow = new google.maps.InfoWindow( {
-                maxWidth: 300
-            } );
-
-            styleInfowindow( infoWindow );
-            addMarkerClickListener( map, marker, textContent, infoWindow );
-        } else {
-            addMarkerClickListener( map, marker );
-        }
-
-    }
-
     function createMarker( map, location ) {
         var marker = new google.maps.Marker({
             map: map
@@ -278,36 +114,200 @@ function initMap() {
         showMarkerForm( map, marker );
         markers.push( markerObject );
     }
+}
 
-    function validateMarkerForm( markerForm ) {
-        var formFields = markerForm.find( '.marker-field' );
-        for ( var i = 0; i < formFields.length; i++ ) {
-            var field = $( formFields[ i ] );
-            validateField( field, false );
-            field.blur( function ( e ) {
-                validateField( $( e.target ), true );
-            } );
+function styleInfowindow( infowindow ) {
+    google.maps.event.addListener( infowindow, 'domready', function() {
+
+        // Reference to the DIV that wraps the bottom of infowindow
+        var iwOuter = $('.gm-style-iw');
+        iwOuter.parent().css({'width': iwOuter.css('width'), 'height': iwOuter.css('height') });
+
+        /* Since this div is in a position prior to .gm-div style-iw.
+         * We use jQuery and create a iwBackground variable,
+         * and took advantage of the existing reference .gm-style-iw for the previous div with .prev().
+         */
+        var iwBackground = iwOuter.prev();
+
+        // Removes background shadow DIV
+        iwBackground.children(':nth-child(2)').css({'display' : 'none'});
+
+        // Removes white background DIV
+        iwBackground.children(':nth-child(4)').css({'display' : 'none'});
+
+        // Changes the desired tail shadow color.
+        iwBackground.children(':nth-child(3)').find('div').children().css({'box-shadow': 'rgba(72, 181, 233, 0.6) 0px 1px 6px', 'z-index' : '1'});
+
+        // Reference to the div that groups the close button elements.
+        var iwCloseBtn = iwOuter.next();
+
+        // Apply the desired effect to the close button
+        iwCloseBtn.css({opacity: '1', top: '0' , right: '-35px', border: '7px solid #417DF1', 'border-radius': '13px', 'box-shadow': '0 0 5px #3990B9'});
+
+        // If the content of infowindow not exceed the set maximum height, then the gradient is removed.
+        if($('#lb-gmaps-marker-description').height() < 140){
+            $('#lb-gmaps-marker-gradient ').css({display: 'none'});
         }
 
-        function handleSaveButton() {
-            if( formFields.length === $( '.valid' ).length ) {
-                $( '#save-button' ).prop( 'disabled', false );
-            } else {
-                $( '#save-button' ).prop( 'disabled', true );
+        // The API automatically applies 0.7 opacity to the button after the mouseout event. This function reverses this event to the desired value.
+        iwCloseBtn.mouseout(function(){
+            $(this).css({opacity: '1'});
+        });
+    });
+}
+
+function showMarkerForm( map, marker ) {
+    var markerObject = {
+        uniqueness: marker.uniqueness,
+        post_id: post.ID,
+        lat: marker.position.lat(),
+        lng: marker.position.lng()
+    };
+
+    var markerForm = $( views.form );
+
+    if( marker.hasOwnProperty( 'name' ) ) {
+        markerForm.find( '#marker_name' ).val( marker.name );
+    }
+    if( marker.hasOwnProperty( 'content' ) ) {
+        markerForm.find( '#marker_description' ).val( marker.content );
+    }
+    if( ! marker.hasOwnProperty( 'name' ) && ! marker.hasOwnProperty( 'content' ) ) {
+        $( '<button type="button" id="delete-button">Delete Marker</button>' ).insertAfter( markerForm.find( '#save-button' ) );
+    }
+
+
+    markerForm.insertAfter( '#lb-gmaps-fields' );
+
+    validateMarkerForm( markerForm );
+
+    $( '#cancel-button' ).on( 'click', function () {
+        markerForm.remove();
+        displayInfoWindow( map, marker );
+    } );
+
+    $( '#save-button' ).on( 'click', function () {
+        markerObject.name = $.trim( $( '#marker_name' ).val() );
+        markerObject.content = $.trim( $( '#marker_description' ).val() );
+        markerForm.remove();
+        $.ajax( {
+            type: "POST",
+            url: admin.ajaxURL,
+            data: {
+                action: 'save_marker_data',
+                marker: markerObject
             }
-        }
+        } ).then( function (data) {
+            if( data ) {
+                marker.name = markerObject.name;
+                marker.content = markerObject.content;
+                displayInfoWindow( map, marker );
+            }
+        } );
+    } );
 
-        function validateField( field, withErrors ) {
-            if( '' === field.val() ) {
-                field.removeClass( 'valid' );
-                if( ! field.siblings( '.marker-error' ).length && withErrors ) {
-                    field.parent().append( '<div class="marker-error">' + errors.emptyField + '</div>' );
+    $( '#delete-button' ).on( 'click', function () {
+        marker.setMap( null );
+        markerForm.remove();
+    } )
+}
+
+function addMarkerClickListener( map, marker, content, infoWindow ) {
+    google.maps.event.clearListeners( marker, 'click' );
+    google.maps.event.addListener( marker, 'click', ( function( marker, content, infoWindow ) {
+        return function() {
+            if( undefined !== infoWindow && undefined !== infoWindow && undefined !== content ) {
+                infoWindow.setContent( content );
+                if( undefined === infoWindow.getMap() || null === infoWindow.getMap() ) {
+                    infoWindow.open( map, marker );
                 }
+
+                $( '#edit-lb-gmaps-marker' ).on( 'click', function (  ) {
+                    $( '.gm-style-iw' ).parent().remove();
+                    showMarkerForm( map, marker );
+                } );
+
+                $(' #delete-lb-gmaps-marker ').on( 'click', function () {
+                    $( '.gm-style-iw' ).parent().remove();
+                    google.maps.event.clearListeners( marker, 'click' );
+                    var markerObject = {
+                        uniqueness: marker.uniqueness,
+                        lat: marker.position.lat(),
+                        lng: marker.position.lng()
+                    };
+                    $.ajax( {
+                        type: 'POST',
+                        url: admin.ajaxURL,
+                        data: {
+                            action: 'delete_marker_data',
+                            marker: markerObject
+                        }
+                    } ).then( function ( data ) {
+                        if( data ) {
+                            marker.setMap( null );
+                        }
+                    } )
+                } )
             } else {
-                field.addClass( 'valid' );
-                field.siblings( '.marker-error' ).remove();
+                showMarkerForm( map, marker );
             }
-            handleSaveButton();
+
+        };
+    } )( marker, content, infoWindow) );
+}
+
+function displayInfoWindow( map, marker ) {
+    var content = $( views.infoBox );
+    content.find( '#lb-gmaps-marker-description' ).append( '<span id="edit-lb-gmaps-marker">Edit Marker Info</span>' );
+    content.find( '#lb-gmaps-marker-description' ).append( '<span id="delete-lb-gmaps-marker">Delete Marker</span>' );
+    if( null !== marker.name && null !== marker.content && undefined !== marker.name && undefined !== marker.content) {
+        if( null !== marker.name ) {
+            content.find( '#lb-gmaps-marker-name p' ).text( marker.name );
         }
+        if( null !== marker.content ) {
+            content.find( '#lb-gmaps-marker-description p' ).text( marker.content );
+        }
+        var textContent = content[0].outerHTML;
+        var infoWindow = new google.maps.InfoWindow( {
+            maxWidth: 300
+        } );
+
+        styleInfowindow( infoWindow );
+        addMarkerClickListener( map, marker, textContent, infoWindow );
+    } else {
+        addMarkerClickListener( map, marker );
+    }
+
+}
+
+function validateMarkerForm( markerForm ) {
+    var formFields = markerForm.find( '.marker-field' );
+    for ( var i = 0; i < formFields.length; i++ ) {
+        var field = $( formFields[ i ] );
+        validateField( field, false );
+        field.blur( function ( e ) {
+            validateField( $( e.target ), true );
+        } );
+    }
+
+    function handleSaveButton() {
+        if( formFields.length === $( '.valid' ).length ) {
+            $( '#save-button' ).prop( 'disabled', false );
+        } else {
+            $( '#save-button' ).prop( 'disabled', true );
+        }
+    }
+
+    function validateField( field, withErrors ) {
+        if( '' === field.val() ) {
+            field.removeClass( 'valid' );
+            if( ! field.siblings( '.marker-error' ).length && withErrors ) {
+                field.parent().append( '<div class="marker-error">' + errors.emptyField + '</div>' );
+            }
+        } else {
+            field.addClass( 'valid' );
+            field.siblings( '.marker-error' ).remove();
+        }
+        handleSaveButton();
     }
 }
--- includes/metabox/lb_gmaps_metabox.php
+++ includes/metabox/lb_gmaps_metabox.php
@@ -1,12 +1,101 @@
 <div id="lb-gmaps-metabox">
 	<div id="lb-gmaps-fields">
 		<div class="lb-gmaps-form-group">
-			<label for="lb-gmaps-map-marker-popup"><?php echo __( 'Prevent place popup from displaying.', 'lb-gmaps' ) ?></label>
+			<label for="lb-gmaps-map-marker-popup"><?php echo __( 'Prevent place popup from displaying', 'lb-gmaps' ) ?></label>
 			<input type="checkbox" name="lb-gmaps-map-marker-popup" id="lb-gmaps-map-marker-popup">
 		</div>
 		<div class="lb-gmaps-form-group">
 			<label for="lb-gmaps-map-markers"><?php echo __( 'Search Places', 'lb-gmaps' ) ?></label>
 			<input type="text" name="lb-gmaps-map-markers" id="lb-gmaps-map-markers">
+		</div>
+		<div class="lb-gmaps-form-group">
+			<label for="lb-gmaps-map-zoom"><?php echo __( 'Zoom', 'lb-gmaps' ) ?></label>
+			<input type="number" name="lb-gmaps-map-zoom" id="lb-gmaps-map-zoom" placeholder="<?php echo __( 'Enter map zoom', 'lb-gmaps' ) ?>">
+		</div>
+		<div class="lb-gmaps-form-group">
+			<label class="map-controls" for="lb-gmaps-map-scale-control"><?php echo __( 'Scale Control', 'lb-gmaps' ) ?></label>
+			<input type="checkbox" name="lb-gmaps-map-scale-control" id="lb-gmaps-map-scale-control">
+		</div>
+		<div class="lb-gmaps-form-group">
+			<label class="map-controls" for="lb-gmaps-map-zoom-control"><?php echo __( 'Zoom Control', 'lb-gmaps' ) ?></label>
+			<select name="lb-gmaps-map-zoom-control" id="lb-gmaps-map-zoom-control">
+				<option value="choose">Choose...</option>
+				<option value="TOP_LEFT">Top Left</option>
+				<option value="TOP_RIGHT">Top Right</option>
+				<option value="TOP_CENTER">Top Center</option>
+				<option value="LEFT_TOP">Left Top</option>
+				<option value="LEFT_CENTER">Left Center</option>
+				<option value="LEFT_BOTTOM">Left Bottom</option>
+				<option value="RIGHT_TOP">Right Top</option>
+				<option value="RIGHT_CENTER">Right Center</option>
+				<option value="RIGHT_BOTTOM">Right Bottom</option>
+				<option value="BOTTOM_LEFT">Bottom Left</option>
+				<option value="BOTTOM_RIGHT">Bottom Right</option>
+				<option value="BOTTOM_CENTER">Bottom Center</option>
+			</select>
+		</div>
+		<div class="lb-gmaps-form-group">
+			<label class="map-controls" for="lb-gmaps-map-street-view-control"><?php echo __( 'Street View Control', 'lb-gmaps' ) ?></label>
+			<select name="lb-gmaps-map-street-view-control" id="lb-gmaps-map-street-view-control">
+				<option value="choose">Choose...</option>
+				<option value="TOP_LEFT">Top Left</option>
+				<option value="TOP_RIGHT">Top Right</option>
+				<option value="TOP_CENTER">Top Center</option>
+				<option value="LEFT_TOP">Left Top</option>
+				<option value="LEFT_CENTER">Left Center</option>
+				<option value="LEFT_BOTTOM">Left Bottom</option>
+				<option value="RIGHT_TOP">Right Top</option>
+				<option value="RIGHT_CENTER">Right Center</option>
+				<option value="RIGHT_BOTTOM">Right Bottom</option>
+				<option value="BOTTOM_LEFT">Bottom Left</option>
+				<option value="BOTTOM_RIGHT">Bottom Right</option>
+				<option value="BOTTOM_CENTER">Bottom Center</option>
+			</select>
+		</div>
+		<div class="lb-gmaps-form-group">
+			<label class="map-controls" for="lb-gmaps-map-rotate-control"><?php echo __( 'Rotate Control', 'lb-gmaps' ) ?></label>
+			<select name="lb-gmaps-map-rotate-control" id="lb-gmaps-map-rotate-control">
+				<option value="choose">Choose...</option>
+				<option value="TOP_LEFT">Top Left</option>
+				<option value="TOP_RIGHT">Top Right</option>
+				<option value="TOP_CENTER">Top Center</option>
+				<option value="LEFT_TOP">Left Top</option>
+				<option value="LEFT_CENTER">Left Center</option>
+				<option value="LEFT_BOTTOM">Left Bottom</option>
+				<option value="RIGHT_TOP">Right Top</option>
+				<option value="RIGHT_CENTER">Right Center</option>
+				<option value="RIGHT_BOTTOM">Right Bottom</option>
+				<option value="BOTTOM_LEFT">Bottom Left</option>
+				<option value="BOTTOM_RIGHT">Bottom Right</option>
+				<option value="BOTTOM_CENTER">Bottom Center</option>
+			</select>
+		</div>
+		<div class="lb-gmaps-form-group">
+			<label class="map-controls" for="lb-gmaps-map-fullscreen-control"><?php echo __( 'Fullscreen Control', 'lb-gmaps' ) ?></label>
+			<select name="lb-gmaps-map-fullscreen-control" id="lb-gmaps-map-fullscreen-control">
+				<option value="choose">Choose...</option>
+				<option value="TOP_LEFT">Top Left</option>
+				<option value="TOP_RIGHT">Top Right</option>
+				<option value="TOP_CENTER">Top Center</option>
+				<option value="LEFT_TOP">Left Top</option>
+				<option value="LEFT_CENTER">Left Center</option>
+				<option value="LEFT_BOTTOM">Left Bottom</option>
+				<option value="RIGHT_TOP">Right Top</option>
+				<option value="RIGHT_CENTER">Right Center</option>
+				<option value="RIGHT_BOTTOM">Right Bottom</option>
+				<option value="BOTTOM_LEFT">Bottom Left</option>
+				<option value="BOTTOM_RIGHT">Bottom Right</option>
+				<option value="BOTTOM_CENTER">Bottom Center</option>
+			</select>
+		</div>
+		<div class="lb-gmaps-form-group">
+			<label class="map-controls" for="lb-gmaps-map-types"><?php echo __( 'Map Types', 'lb-gmaps' ) ?></label>
+			<select name="lb-gmaps-map-types" id="lb-gmaps-map-types" multiple>
+				<option value="roadmap">roadmap</option>
+				<option value="satellite">satellite</option>
+				<option value="hybrid">hybrid</option>
+				<option value="terrain">terrain</option>
+			</select>
 		</div>
 	</div>
 	<div id="lb-gmaps-live-preview"></div>
